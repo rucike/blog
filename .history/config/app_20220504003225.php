cd9a55f5939e4de07eba56445e3ff95527e7fc0e
Rūta P <ruta.plac@gmail.com>
2022-05-04 00:48:55 +0300

    done

--- .history/config/app_20220504003225.php
+++ .history/config/app_20220504003225.php
@@ -69,7 +69,7 @@ return [
     |
     */
 
-    'timezone' => 'Europe/Vilnius',
+    'timezone' => 'date_default_timezone_get()',
 
     /*
     |--------------------------------------------------------------------------
